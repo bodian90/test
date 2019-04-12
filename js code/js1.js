07a2837b5162bbc03a9dfa7d60af11b433be960c
Bohdan <artuhov.bohdan@gmail.com>
2019-04-12 11:56:21 +0200

    это первый коммит

---
+++ js code/js1.js
@@ -0,0 +1,25 @@
+/*let persone = {
+    name: 'John',
+    age: 26
+};
+console.log(persone.name);*/
+
+//alert("hello World");
+
+//let answer = confirm("Are you ready");
+//console.log(answer);
+
+
+let incr = 10,
+    decr = 10;
+console.log(++incr);
+console.log(--decr);
+
+console.log(6%5);
+console.log("2"== 4);
+
+let isbohdan = true;
+let  iskamil = false;
+
+    console.log(isbohdan || iskamil);
+
---
+++ lessons 1/javascript.js
@@ -0,0 +1,89 @@
+
+
+
+//alert("Hello world");
+
+//переменные
+
+/*var LeftBorderWidth = 1;переменная (имя переменной) =(присваиваем значение) 
+let second = 2;
+const pi = 3.14;
+console.log(second);*/
+
+//типы данных
+/*var number =5;
+var string = "Hello world";
+var symb = Symbol ();
+var boolen = true;
+null;   
+undefined;
+var obj = {
+};
+
+console.log(4/0); //infinity
+console.log("hello " + 4); //строка + число = строка
+console.log("hello " * 4); // NaN 
+*/
+/*let persone = {
+    name: "John",
+    age: 12,
+    isMarryed: true
+};*/ //объект состоит из {} и заключает в себе информацию
+//console.log(persone ['isMarryedname']);
+
+//let arr = ['plum.png','orang.jpg', 'bamblbe.png']; /*массив заключается в "[]" и содержит в себе фото*/
+//console.log(arr[2]);
+
+//общение с пользователем 
+
+// alert ("Hello world!") //модальное окно которое приостанавливает все процессы(пока не нажмешь "ок")
+//let answer = confirm('Are you ready?'); //выводит "ок" или "отмена" и показывает булиновое значение("true" or "false")
+//console.log(answer);
+
+//let answer = prompt("Чи тобі вже є 18?" ,  "так"); //команда которая задает вопрос пользователю, и получиный ответ выводит тебе
+//console.log(typeof(answer));//чтобы узнать какой тип данных содержит переменная (typeof)
+
+//Операторы в js
+
+// при сложении двух строк будет строка, при сложение строки с числом будет строка
+//console.log("obj" + "- object");
+
+//инкриментр и дикримент (увеличивает или уменчает число на "1")
+//let incr = 10,
+    //decr = 10;
+
+//incr ++;
+//decr --;
+
+//console.log(incr++);//постфискная форма 
+//console.log(decr--);
+
+//console.log(++incr);//префискная форма 
+//console.log(--decr);
+
+//console.log(incr++);//постфискная форма (возвращает старое значение а потом увеличивает или уменьшает на 1)
+//console.log(decr--);
+
+// % возвращает остаток от деление двух чисел
+
+//console.log(6%4);
+
+//Присваивание (=), Сравнение значений(==), Сравнение типов данных(===)
+//var a = 2; // "=" - присваивает
+//console.log("2"==2); // "==" - сравнивает значения
+//console.log("2"===2);//"==="- сравнивает типы данных
+
+
+//Логические операторы и(&&) , или (||)
+
+/*let isCheked = true,
+    isClose =false;
+
+console.log(isCheked&&isClose);//когда два значения (true) то будет true, когда одно из (false), то будет false
+console.log(isCheked||isClose);//когда одно из значений (true) то всегда будет (true)*/
+
+
+
+
+
+
